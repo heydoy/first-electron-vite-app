19de9b29bf57a4113ced0fe9674d75c0f826da62
김도이 <doy.kim@ahha.ai>
2024-08-12 16:24:14 +0900

    fix: 중복된 ipcHandler 정리

--- src/main/index.ts
+++ src/main/index.ts
@@ -2,7 +2,7 @@ import { app, shell, BrowserWindow, ipcMain } from 'electron'
 import { join } from 'path'
 import { electronApp, optimizer, is } from '@electron-toolkit/utils'
 import icon from '../../resources/icon.png?asset'
-import ipcHandler from './utils'
+import ipcHandler from './utils/ipcHandler'
 
 function createWindow(): void {
   // Create the browser window.
